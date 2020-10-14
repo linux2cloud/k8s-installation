3b598c429ecbc9b2c92cef7908d360c3ae9c59f3
linux2cloud <69761876+linux2cloud@users.noreply.github.com>
2020-10-14 13:09:20 +0530

    Update swap-disable.sh

--- swap-disable.sh
+++ swap-disable.sh
@@ -1,1 +1,2 @@
-for n in master-a master-b worker-a worker-b; do echo "*********$n********"; ssh -qt $n "sed -i '/swap/d' /etc/fstab; swapoff -a; swapon -s"; done
+## Disable swap
+for n in master-a master-b worker-a worker-b; do echo "*********$n********"; ssh -qt $n "sed -i '/swap/d' /etc/fstab; swapoff -a"; done
